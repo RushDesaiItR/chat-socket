4e7edaba692ce4f878d10aca5e2d95062ab84230
Rushikesh Desai <rushikeshmahavirdesaithree@gmail.com>
2021-11-21 10:59:16 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express')
+require('dotenv').config()
 var app = express()
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-const port = 3000
+const port = process.env.PORT || 3800;
 io.on("connection",socket=>{
     console.log("User Connected")
 })
-app.listen(port, ()=> console.log(`http://localhost/${port}`))git
+app.listen(port, ()=> console.log(`http://localhost/${port}`))
