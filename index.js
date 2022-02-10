58daf48ae47b7badfee83d85cf236fcd5d88af56
Rushikesh Desai <rushikeshmahavirdesaithree@gmail.com>
2022-02-10 08:36:02 +0530

    socket changes

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http,{
-  cors:{
-    origin:"*"
-  }
-});
-const port = process.env.PORT || 5000;
+// Libraray
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const { addUser, removeUser, getUser, getRoomUsers } = require("./entity");
+
+// Instances
+const app = express()
+const server = http.createServer(app);
+const io = socketio(server,{cors: { origin: '*' }})
+
+// End point
+app.get('/',(req,res) => {
+  res.json("Api is working");
+})
+
+// Socket
+
+io.on('connect',(socket) => {
+  
+
+  socket.on('join',({user,room},callback) => {
+    console.log(user,room)
+      const {response , error} = addUser({id: socket.id , user:user, room: room})
+
+      console.log(response)
+
+      if(error) {
+        callback(error)
+        return;
+      }
+      socket.join(response.room);
+      socket.emit('message', { user: 'admin' , text: `Welcome ${response.user} ` });
+      socket.broadcast.to(response.room).emit('message', { user: 'admin', text : `${response.user} has joined` })
+
+      io.to(response.room).emit('roomMembers', getRoomUsers(response.room))
+  })
+
+  socket.on('sendMessage',(message,callback) => {
+    
+      const user = getUser(socket.id)
+
+      io.to(user.room).emit('message',{ user: user.user, text : message })
+
+      callback()
+  })
 
 
-io.on('connection', (socket) => {
-  console.log("socket connected sucessfully")
-  socket.on('chat', msg => {
-    console.log(msg)
-    io.emit('chat', msg);
-  });
-});
+ 
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+  socket.on('disconnect',() => {
+    console.log("User disconnected");
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message',{ user: 'admin', text : `${user.user} has left` })
+    }
+  })
+
+  
+
+
+  
+})
+
+
+
+
+server.listen(8000,() => console.log('Server started on 8000'))
